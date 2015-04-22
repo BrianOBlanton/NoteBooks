387cc9102fa40d751a1e3ac4ea779b0faeeec319
BrianOBlanton <brian_blanton@renci.org>
2015-04-22 16:13:18 -0400

    updates for baseman examples

--- JPL-ECCO2_SSH_Example.R
+++ JPL-ECCO2_SSH_Example.R
@@ -7,8 +7,8 @@ library(raster)
 library(rasterVis)
 
 
-#url='http://opendap.renci.org:1935/thredds/dodsC/oedata/SSH.1440x720.20140917.nc'
-url='http://localhost:8080/thredds/dodsC/DBTest/netCDF/SSH.1440x720.20140917.nc'
+url='http://opendap.renci.org:1935/thredds/dodsC/oedata/SSH.1440x720.20140917.nc'
+#url='http://localhost:8080/thredds/dodsC/DBTest/netCDF/SSH.1440x720.20140917.nc'
 
 
 nc=open.ncdf(url)
@@ -23,12 +23,15 @@ ssh=get.var.ncdf(nc, "SSH")
 temp11 <- ssh[ , ] 
 
 # plot as image
-image(lon,lat,temp11) 
-data(wrld_simpl)
-plot(wrld_simpl,add=TRUE)
+#image(lon,lat,temp11) 
+#data(wrld_simpl)
+#plot(wrld_simpl,add=TRUE)
 
 # grid <- expand.grid(xlon=lon, ylat=lat)
 # xyz <- cbind(grid, temp11)
 # pj <- "+proj=lcc +lat_1=25.00 +lat_2=60.00 +lat_0=42.5 +lon_0=-100.00 +x_0=0 +y_0=0 +ellps=GRS80 +units=m +no_defs"
 
 # rast.ssh <- rasterFromXYZ(xyz, crs=pj) 
+
+f <- '/Users/bblanton/Desktop/Dropbox/bb/SSH.1440x720.20140917.nc'
+
