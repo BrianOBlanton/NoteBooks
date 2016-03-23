c9f25f46609cb190e92c8fdae39917f3c4d75699
BrianOBlanton <brian_blanton@renci.org>
2016-03-23 12:02:20 -0400

    moved RSM computational routines to external file
    put central_ckv and sur_model  into external central_ckv.py file
    imported by RSM notebook

--- RSM_implementation_in_Python.py
+++ RSM_implementation_in_Python.py
@@ -17,6 +17,8 @@ import numpy as np
 import numpy.matlib as ml
 import numpy.linalg as la
 
+from central_ckv import central_ckv
+
 
 # In[2]:
 
@@ -31,10 +33,10 @@ print f
 
 # In[3]:
 
-print f.keys()
+#print f.keys()
 #for name in f:
 #     print name
-print "P" in f
+#print "P" in f
 c=np.squeeze(f['c'][:])
 k=np.squeeze(f['k'][:])
 Nd=np.squeeze(f['n_d'][:])
@@ -133,150 +135,19 @@ def master(p1,p2,p3,p4,p5,p6):
 
 # In[5]:
 
-def central_ckv (P,R,c,k,weights,Nd,index,xtest):
-    
-    one=np.ones(1)
-    one=np.expand_dims(one,axis=1)
-
-    P=np.transpose(P)
-    ni=index.shape[0]
-    index=(index-1).astype(int)    
-    
-    # normalization of model parameters 
-    v_aux=np.diag(1/np.mean(P,axis=1))
-    
-    # convert model parameters to normalised space
-    Normalized_P=np.dot(v_aux,P)
-    
-    NSupportPoints=Normalized_P.shape[1];  
-    Weight_Matrix=np.diag(weights/np.std(Normalized_P,axis=1,ddof=1)); 
-    mean_R=np.mean(R,axis=0); 
-    mean_R=np.expand_dims(mean_R,axis=1)
-    mean_R=np.transpose(mean_R)
-
-    std_R=np.std(R,axis=0,ddof=1);
-    std_R=np.expand_dims(std_R,axis=1)
-
-    temp=ml.repmat(mean_R,NSupportPoints,1)
-    numer=R-temp
-
-    S=np.array(1/std_R)
-    S=np.diag(np.squeeze(S),k=0)
-    
-    Normalized_R=np.dot(numer,S);
-
-    BasisFxns=np.array([])
-    for i in range(0,NSupportPoints): 
-        
-        for j in range(0,ni):
-            ii=index[j]
-            temp=np.array([Normalized_P[ii,i]*Normalized_P[index[j:ni],i]])
-            temp=np.transpose(temp)
-
-            if (j == 0):
-                aux=np.array(temp)
-            else:
-                aux=np.vstack((aux, temp))
-                
-        temp=Normalized_P[:,i] 
-        temp=np.expand_dims(temp,axis=0)
-        aux=np.transpose(aux)
-        temp=np.hstack((one,temp, aux))
-      
-        if (i==0):
-            BasisFxns=temp
-        else:
-            BasisFxns=np.vstack((BasisFxns,temp))
-                        
-    zhat=sur_model(xtest,v_aux,Weight_Matrix,Normalized_P,Nd,c,k,BasisFxns,Normalized_R,mean_R,std_R,NSupportPoints,index)   
-    return zhat
-
-
-# In[6]:
-
-def sur_model(x,v_aux,Weight_Matrix,Normalized_P,Nd,c,k,BasisFxns,Normalized_R,mean_R,std_R,NSupportPoints,index):
-
-    one=np.ones(1)
-    one=np.expand_dims(one,axis=1)
-    
-    # normalize input vector x
-    Normalized_X=np.dot(v_aux,x)
-    Normalized_X=np.expand_dims(Normalized_X,axis=1)
-
-    Parameter_Diffs=ml.repmat(Normalized_X,1,NSupportPoints)-Normalized_P
-    Parameter_Diffs_Weighted=np.dot(Weight_Matrix,Parameter_Diffs)
-    
-    temp=np.square(Parameter_Diffs_Weighted,Parameter_Diffs_Weighted)
-    temp=np.sum(temp,axis=0)
-    temp=np.expand_dims(temp,axis=0)
-    Distance=np.sqrt(temp);
-
-    SortedDistanceIndex=np.argsort(Distance); 
-    SortedDistance=Distance[0,SortedDistanceIndex]    
-    D=SortedDistance[0,Nd.astype(int)-1]
-    SelectedStorms=SortedDistanceIndex[0,:Nd.astype(int)]
-
-    f=np.exp(-np.power(1/c,k))
-    denom=(1-f)
-    numer=np.exp(-(np.power(Distance/D/c,k)))-f
-    aux1=numer/denom;
-
-    W=np.diag(aux1[0,SelectedStorms]); 
-
-    Ba=BasisFxns[SelectedStorms,:];
-    
-    ni=index.shape[0]
-
-    for j in range(0,ni):
-        ii=index[j]
-        temp=np.array([Normalized_X[ii]*Normalized_X[index[j:ni]]])
-        temp=np.squeeze(temp)
-        temp=np.expand_dims(temp,axis=1)
-
-        if (j == 0):
-            aux=np.array(temp)
-        else:
-            aux=np.vstack((aux, temp))
-
-    b=np.hstack((one,np.transpose(Normalized_X), np.transpose(aux)))
-
-    # auxiliary matrices
-    L=np.dot(np.transpose(Ba),W)
-    M=np.dot(L,Ba) 
-    
-    # auxM are the b'*inv(M)*L coefficients in eqn 24 of Taflanidis 2012
-    Mi=la.inv(M)
-    temp=np.dot(b,Mi)
-    auxM=np.dot(temp,L);
-
-    Fi=Normalized_R[SelectedStorms,:];
-
-    temp=np.dot(auxM,Fi)
-    std_R=np.transpose(std_R)
-
-    temp2=temp[0,:]*std_R
-    f=temp2+mean_R
-    f=np.squeeze(f)
-    
-    return f
-
-
-# In[7]:
-
-# evaluate the model at xtest parameters
-#zhat=master(p1,p2,p3,p4,p5,p6)
-
-
-# In[8]:
-
 #from __future__ import print_function # for python 2
 from ipywidgets import interact, interactive, fixed
 import ipywidgets as widgets
 
 
-# In[10]:
+# In[6]:
 
 interact(master,          p1=widgets.FloatSlider(min=p1min,max=p1max,step=dp1,value=p1),         p2=widgets.FloatSlider(min=p2min,max=p2max,step=dp2,value=p2),         p3=widgets.FloatSlider(min=p3min,max=p3max,step=dp3,value=p3),         p4=widgets.FloatSlider(min=p4min,max=p4max,step=dp4,value=p4),         p5=widgets.FloatSlider(min=p5min,max=p5max,step=dp5,value=p5),         p6=widgets.FloatSlider(min=p6min,max=p6max,step=dp6,value=p6))
+
+
+# In[ ]:
+
+
 
 
 # In[ ]:
