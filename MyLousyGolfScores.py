18d389737c0ff125a0c23692ba71b165347d73c1
BrianOBlanton <Brian_Blanton@Renci.Org>
2016-04-05 10:07:43 -0400

    updated golf data

--- MyLousyGolfScores.py
+++ MyLousyGolfScores.py
@@ -1,7 +1,7 @@
 
 # coding: utf-8
 
-# In[80]:
+# In[191]:
 
 import numpy as np
 import tables as PyT
@@ -10,21 +10,22 @@ import matplotlib.pyplot as plt
 get_ipython().magic(u'matplotlib inline')
 
 
-# In[84]:
+# In[192]:
 
-f=pd.read_csv('golf_data.csv',parse_dates={'dt':[0]})
+f=pd.read_csv('golf_data.csv',parse_dates={'dt':[18]})
 
 
-# In[178]:
+# In[199]:
 
 NumberOfRounds=f.shape[0]/9
+print "Number of 9-hole rounds = " + str(NumberOfRounds)
 # slopes/ratings (divided by 2 for 18-hole courses)
 # Hillandale slope = 112, rating = 64.5/2
 # Occoneechee slope = 124, rating 68.6/2
 # Twin Lakes slope = 113, rating = 34.2
-ratings = {'Hillandale Golf Course': 64.5/2, 'Occoneechee Golf Club': 68.6/2, 'Twin Lakes Golf Course': 34.2}
-slopes  = {'Hillandale Golf Course': 112,    'Occoneechee Golf Club': 124, 'Twin Lakes Golf Course': 113}
-
+# Lakeshore slope = 117, rating = 67.3/2
+ratings = {'Hillandale Golf Course': 64.5/2, 'Occoneechee Golf Club': 68.6/2, 'Twin Lakes Golf Course': 34.2, 'Lakeshore Golf Course': 67.3/2}
+slopes  = {'Hillandale Golf Course': 112,    'Occoneechee Golf Club': 124, 'Twin Lakes Golf Course': 113, 'Lakeshore Golf Course': 117}
 
 Courses=np.flipud(np.reshape(f.Course,(NumberOfRounds,9)))
 Courses=Courses[:,0]
@@ -35,7 +36,7 @@ for i in range(0,Courses.shape[0]):
     CourseRatings[i]=ratings[Courses[i]]
 
 
-# In[179]:
+# In[200]:
 
 PerHoleScores=np.flipud(np.reshape(f.Score,(NumberOfRounds,9)))
 RoundScores=np.sum(PerHoleScores,axis=1)
@@ -43,7 +44,7 @@ t=np.flipud(np.reshape(f.dt,(NumberOfRounds,9)))
 dates=t[:,0]
 
 
-# In[180]:
+# In[201]:
 
 regression = np.polyfit(range(0,NumberOfRounds), RoundScores, 1)
 r_x, r_y = zip(*((i, i*regression[0] + regression[1]) for i in range(NumberOfRounds)))
@@ -51,7 +52,7 @@ regression2 = np.polyfit(range(1,NumberOfRounds), RoundScores[1:], 1)
 r2_x, r2_y = zip(*((i, i*regression2[0] + regression2[1]) for i in range(NumberOfRounds)))
 
 
-# In[181]:
+# In[202]:
 
 plt.figure(figsize=(10,6))
 plt.plot(RoundScores,'ro-')
@@ -67,12 +68,12 @@ lstr.append('Excluding first (83)')
 plt.legend(lstr, fontsize=12);
 
 
-# In[182]:
+# In[203]:
 
 #plt.hist(RoundScores)
 
 
-# In[183]:
+# In[204]:
 
 # adjusted gross score
 # max of 9 on any hole
@@ -83,12 +84,12 @@ regression_adj = np.polyfit(range(0,NumberOfRounds), RoundScores_adj, 1)
 r_x_adj, r_y_adj = zip(*((i, i*regression_adj[0] + regression_adj[1]) for i in range(NumberOfRounds)))
 
 
-# In[184]:
+# In[213]:
 
 plt.figure(figsize=(10,6))
 plt.plot(RoundScores,'ro-')
 plt.plot(RoundScores_adj,'g*-')
-plt.plot(64.5*np.ones([NumberOfRounds+1,1]),'b-')
+#plt.plot(64.5*np.ones([NumberOfRounds+1,1]),'b-')
 plt.ylabel("9-hole score",fontsize=14)
 plt.xlabel("Round number",fontsize=14)
 plt.grid(True)
@@ -97,40 +98,41 @@ plt.plot(r_x_adj, r_y_adj, color="green")
 lstr=[]
 lstr.append('Raw Scores')
 lstr.append('Adjusted Scores')
-lstr.append('Hillandale Course Rating')
+#lstr.append('Hillandale Course Rating')
 #lstr.append('Excluding first (83)')
 plt.legend(lstr, fontsize=12);
 
 
-# In[185]:
+# In[215]:
 
 ## Handicap differential = (Adjusted Gross Score - Course Rating) x 113 ÷ Slope Rating
 ## https://www.golfnow.com/courses/1033123-hillandale-golf-course-details
 
-hd=(RoundScores_adj-CourseRatings)*113/112
+HandicapDifferential=(RoundScores_adj-CourseRatings)*113/112
 # sort hd, and take 10 best
-hd=np.sort(hd)
-hd=hd[-10:-1]
+HandicapDifferential=np.sort(HandicapDifferential)
+HandicapDifferential=HandicapDifferential[-10:-1]
 #hd=np.trunc(hd*10)/10
 
 
-# In[186]:
+# In[216]:
 
-ahd=np.mean(hd)
-print ahd
-
-
-# In[187]:
-
-HandiCapIndex=np.trunc(.96*ahd*10)/10
+MeanHandicapDifferential=np.mean(HandicapDifferential)
 
 
-# In[188]:
+# In[217]:
+
+HandiCapIndex=np.trunc(.96*MeanHandicapDifferential*10)/10
+print "Handicap Index = " + str(HandiCapIndex)
+
+
+# In[218]:
 
 # hillandale hc
 print 'Hillandale HC = ' + str(int(np.round(HandiCapIndex*slopes['Hillandale Golf Course']/113)))
 print 'Occoneechee HC = ' + str(int(np.round(HandiCapIndex*slopes['Occoneechee Golf Club']/113)))
 print 'Twin Lakes HC = ' + str(int(np.round(HandiCapIndex*slopes['Twin Lakes Golf Course']/113)))
+print 'Lakeshore HC = ' + str(int(np.round(HandiCapIndex*slopes['Lakeshore Golf Course']/113)))
 
 
 # In[ ]:
