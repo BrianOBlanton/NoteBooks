5de0782bc6bffb5ac44d0e5c151cdd4f54cf97f3
BrianOBlanton <brian_blanton@renci.org>
2016-04-06 10:12:21 -0400

    added nice map code to HYCOM example

--- Hycom_Python.py
+++ Hycom_Python.py
@@ -3,20 +3,19 @@
 
 # # Example of working with HYCOM output available on the hycom.org tds
 
-# In[10]:
+# In[1]:
 
 # Import packages we'll need
 import netCDF4
 import matplotlib.pyplot as plt
 import numpy as np 
 import datetime as dt  # Python standard library datetime  module
-import math as math
 from pylab import quiver
 from mpl_toolkits.basemap import Basemap
 get_ipython().magic(u'matplotlib inline')
 
 
-# In[11]:
+# In[2]:
 
 def find_nearest(array,value):
     idx = (np.abs(array-value)).argmin()
@@ -26,7 +25,7 @@ def find_nearest_idx(array,value):
     return idx
 
 
-# In[12]:
+# In[3]:
 
 # open pipes to hycom output
 url='http://tds.hycom.org/thredds/dodsC/GLBa0.08/expt_91.1/2015'
@@ -34,12 +33,12 @@ nc=netCDF4.Dataset(url)
 print 'Available variables are: ' +  str(nc.variables.keys())
 
 
-# In[13]:
+# In[4]:
 
 print nc
 
 
-# In[14]:
+# In[5]:
 
 # create variables for u,v,x,y,depth
 lon=nc.variables['Longitude']
@@ -69,7 +68,7 @@ lat_d=lat_d[ilat1:ilat2]
 lons,lats = np.meshgrid(lon_d,lat_d)
 
 
-# In[15]:
+# In[6]:
 
 # specify time level
 it=1
@@ -79,7 +78,7 @@ time=Time[it]
 print time
 
 
-# In[16]:
+# In[7]:
 
 # 
 var1_name='ssh'
@@ -90,11 +89,11 @@ vmin=var1_d.min()
 vmax=var1_d.max()
 
 # This min/max calc won't work in general, but is OK for data relatively well distributed about 0
-vminmax=math.ceil(2*max(abs(vmin),abs(vmax)))/2
+vminmax=np.ceil(2*max(abs(vmin),abs(vmax)))/2
 print vmin,vmax,vminmax
 
 
-# In[17]:
+# In[8]:
 
 ilev=1   # specify vertical level to extract
 uvel_name='u'
@@ -111,7 +110,7 @@ vvel_max=vvel_d.max()
 print vvel_min,vvel_max
 
 
-# In[21]:
+# In[9]:
 
 # Let's plot ssh, for a test
 fig = plt.figure(figsize=(18,9), dpi=144)
@@ -135,6 +134,57 @@ cbax = fig.add_axes([0.80, 0.1, 0.05, 0.8])
 cb = plt.colorbar(pc, cax=cbax,  orientation='vertical')
 cb.set_label(var1.units, fontsize=24)
 cb.ax.tick_params(axis='both', which='major', labelsize=20)
+
+
+# In[24]:
+
+llcrnrlon=np.floor(np.min(lon_d))  # lower-left corner, lon
+llcrnrlat=np.floor(np.min(lat_d))  # lower-left corner, lat
+urcrnrlon=np.ceil(np.max(lon_d))   # upper-right corner, lon
+urcrnrlat=np.ceil(np.max(lat_d))   # upper-right corner, lat
+lon_0    =np.mean(lon_d)        # center of map domain (in degrees).
+lat_1    =np.mean(lat_d)        # first standard parallel for lambert conformal
+print lon_0,lat_1
+
+fig = plt.figure(figsize=(18,9), dpi=72)
+ax = fig.add_axes([0.1, 0.1, 0.8, 0.8])
+
+#m = Basemap(projection='kav7',lon_0=-80,resolution='l')
+
+map = Basemap(llcrnrlon=llcrnrlon,
+            llcrnrlat=llcrnrlat,
+            urcrnrlon=urcrnrlon,
+            urcrnrlat=urcrnrlat,
+            projection='lcc',
+            resolution ='i',
+            area_thresh=1000.,
+            lat_1=lat_1,
+            lon_0=lon_0)
+
+map.drawmapboundary(fill_color='0.8')
+map.drawcoastlines(linewidth=1.)
+map.fillcontinents(color='0.8')
+map.drawparallels(np.arange(llcrnrlat,urcrnrlat,2),labels=[2,0,2,0],fontsize=12)
+map.drawmeridians(np.arange(llcrnrlon,urcrnrlon,2),labels=[0,0,0,2],fontsize=12)
+
+x,y=map(lons,lats)
+
+pc=plt.pcolormesh(x, y, var1_d, cmap=plt.cm.jet, 
+                  vmin=-vminmax, vmax=vminmax)
+Q = quiver(x[::ivecstride, ::ivecstride],y[::ivecstride, ::ivecstride], 
+           uvel_d[::ivecstride , ::ivecstride], vvel_d[::ivecstride, ::ivecstride], 
+           angles='xy',scale_units='xy')
+print lons[0,0] , x[0,0]
+print lats[0,0] , y[0,0]
+print var1_d[0,0] 
+
+cb = map.colorbar(pc,"right", size="5%", pad="10%")
+cb.set_label(var1.units, fontsize=24)
+cb.ax.tick_params(axis='both', which='major', labelsize=12)
+
+plt.title("%s\n%s @ %s" % (nc.title, var1_name, time), fontsize=30)
+plt.show()
+
 
 
 # In[ ]:
